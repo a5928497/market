628b710a2cc6d450292368f94ea5a1a0b74976e8
a5928497 <61722615@qq.com>
2019-12-31 18:37:06 +0800

    ready

---
+++ src/main/java/com/yukoon/market/entities/Tenant.java
@@ -0,0 +1,24 @@
+package com.yukoon.market.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Tenant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+}
