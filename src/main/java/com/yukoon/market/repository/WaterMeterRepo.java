9e8c4e152b3fe392358851ccf61505c8c7ab0b7d
a5928497 <61722615@qq.com>
2020-01-14 18:04:50 +0800

    developing

--- src/main/java/com/yukoon/market/repository/WaterMeterRepo.java
+++ src/main/java/com/yukoon/market/repository/WaterMeterRepo.java
@@ -1,8 +1,15 @@
 package com.yukoon.market.repository;
 
 import com.yukoon.market.entities.EleMeter;
+import com.yukoon.market.entities.WaterMeter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface WaterMeterRepo extends JpaRepository<EleMeter,Integer> {
+import java.util.List;
 
+public interface WaterMeterRepo extends JpaRepository<WaterMeter, Integer> {
+
+    @Query("select w from WaterMeter w where w.market.id = :marketId")
+    public List<WaterMeter> findAllByMarketId(@Param("marketId") Integer marketId);
 }
--- src/main/java/com/yukoon/market/services/WaterMeterService.java
+++ src/main/java/com/yukoon/market/services/WaterMeterService.java
@@ -1,7 +1,27 @@
 package com.yukoon.market.services;
 
+import com.yukoon.market.entities.WaterMeter;
+import com.yukoon.market.repository.WaterMeterRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WaterMeterService {
+    @Autowired
+    private WaterMeterRepo waterMeterRepo;
+
+    //根据市场查找所有电表
+    public List<WaterMeter> findAllByMarketId(Integer marketId) {
+        return waterMeterRepo.findAllByMarketId(marketId);
+    }
+
+    public WaterMeter findById(Integer id) {
+        return waterMeterRepo.findOne(id);
+    }
+
+    public WaterMeter save(WaterMeter eleMeter) {
+        return waterMeterRepo.saveAndFlush(eleMeter);
+    }
 }
