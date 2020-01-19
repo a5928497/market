2d19340c5d181d3a7e913cbd2f368a46a33b8adb
a5928497 <61722615@qq.com>
2020-01-19 17:02:40 +0800

    developing

--- src/main/resources/static/js/backend/tenant_input.js
+++ src/main/resources/static/js/backend/tenant_input.js
@@ -1,3 +1,20 @@
 $(function () {
+    $pure_income = $(".is_pure_income");
 
+    $pure_income.change(function () {
+        var is_pure = $("input[name='is_pure_income']:checked").val();
+       if ("0" == is_pure) {
+           //若选择
+       }else {
+
+       }
+    });
+
+    //返回函数
+    $backBTN = $(".backBTN");
+    $backBTN.click(function () {
+        var uri = $backBTN.attr("back_uri");
+        window.location.replace(uri);
+        return false;
+    })
 })
